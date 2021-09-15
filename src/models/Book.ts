ce566638cf64de3d062d8e4b9a726dbdf7226aa4
Vo Chi Dat <dat.usth2000@gmail.com>
2021-09-15 23:32:57 +0700

    Book Model

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -1,28 +1,39 @@
 import { DatabaseModel } from "./_BaseModel";
+import { Types } from "mongoose";
 import _ from "lodash";
 import { prop, getModelForClass, Ref } from "@typegoose/typegoose";
 import { User } from "./User"
 
 export class Book extends DatabaseModel {
-    @prop()
+    @prop({required: true})
     name!: string;
 
-    @prop()
+    @prop({required: true})
     isnb!: string;
 
-    @prop()
-    description!: string;
+    @prop({required: true})
+    description!: string
 
-    @prop()
-    publisher!: string;
+    @prop({required: true})
+    publisher!: string
 
     @prop({
+        type: () => Types.ObjectId,
         ref: User,
-        foreignField: 'parent',
-        localField: '_id',
+        foreignField: '_id',
+        localField: 'author',
         justOne: true
     })
-    author!: Ref<User>;
+    author!: Ref<User>
+
+    @prop({
+        type: () => Types.ObjectId,
+        ref: User,
+        foreignField: '_id',
+        localField: 'translator',
+        justOne: true
+    })
+    translator!: Ref<User>
 
     sanitise(): SanitisedBook {
         return _.omit(this.toJSON(), "__v");
