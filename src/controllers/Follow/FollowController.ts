66b0eb219a2800527eb89bce1aa79db481d5a6ac
An, Tran Cong Viet <trancongvietan22@gmail.com>
2021-10-27 18:29:48 +0700

    Added Follow Listing

--- src/controllers/Follow/FollowController.ts
+++ src/controllers/Follow/FollowController.ts
@@ -1,45 +1,69 @@
 import {
   Delete,
+  Get,
   Path,
-  Put,
+  Post,
+  Query,
   Request,
   Route,
   Security,
   Tags,
 } from "@tsoa/runtime";
 import type express from "express";
-import { FollowJSON } from "@/models/Follow/Follow";
-import { makeUserFollow, makeUserUnfollow } from "@/services/Follow/Follow";
-import { DeleteResult } from "../_ControllerUtils";
+import { FollowJSON } from "@/models/Follow";
+import {
+  listFollow,
+  makeMeFollowUser,
+  makeMeUnfollowUser,
+} from "@/services/Follow";
+import { DeleteResult, Listing, wrapListingResult } from "../_ControllerUtils";
 
 @Tags("Follow")
 @Route("follow")
 export class FollowController {
   /**
+   * List following. If you need to list following to an user, set toUserId to that user's _id.
+   * If you need to list following from an user, set fromUserId to that user's _id.
+   */
+  @Security("jwt")
+  @Get("/")
+  public async listFollow(
+    @Request() request: express.Request,
+    @Query() limit: number,
+    @Query() cursor?: string,
+    /** can be set to "me" or an arbitrary userId */
+    @Query() fromUserId?: string,
+    /** can be set to "me" or an arbitrary userId */
+    @Query() toUserId?: string
+  ): Promise<Listing<FollowJSON>> {
+    if (fromUserId === "me") {
+      fromUserId = request.user._id.toString();
+    }
+    if (toUserId === "me") {
+      toUserId = request.user._id.toString();
+    }
+    const follows = await listFollow({ limit, cursor, fromUserId, toUserId });
+    return wrapListingResult(follows, request);
+  }
+
+  /**
    * Follow a user
    */
   @Security("jwt")
-  @Put("/{userId}/follow")
-  public async Follow(
+  @Post("/to/{userId}")
+  public async followUser(
     @Request() request: express.Request,
     @Path() userId: string
   ): Promise<FollowJSON> {
-    const follow = await makeUserFollow({
-      follower: request.user,
-      followee: userId,
-    });
-    return follow;
+    return makeMeFollowUser(request.user, userId);
   }
 
   @Security("jwt")
-  @Delete("/{userId}/follow")
-  public async makeUserUnfollow(
+  @Delete("/to/{userId}")
+  public async unfollowUser(
     @Request() request: express.Request,
     @Path() userId: string
   ): Promise<DeleteResult<FollowJSON>> {
-    return makeUserUnfollow({
-      follower: request.user,
-      followee: userId,
-    });
+    return makeMeUnfollowUser(request.user, userId);
   }
 }
